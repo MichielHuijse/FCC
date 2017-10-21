40f9ac6f6d2b67b0d29ece38b05ce9d205664382
huijse <michiel.huijse@gmail.com>
2017-10-21 17:51:37 +0200

    Output works.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,67 +6,108 @@ $(document).ready(function() {
     // all custom jQuery will go here
     var calculationArr = Array();
 
+    $('#AC').click(function(){
+
+        calculationArr = [];
+        $( "#output" ).text("0");
+
+    });
+
     $('#1').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
+        $( "#output" ).text(value);
 
     });
 
     $('#2').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#3').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#4').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#5').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#6').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#7').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#8').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
-
+        $( "#output" ).text(value);
     });
 
     $('#9').click(function(){
         var value = $(this).val();
         calculationArr.push(value);
+        $( "#output" ).text(value);
+    });
 
+    $('#0').click(function(){
+        var value = $(this).val();
+        calculationArr.push(value);
+        $( "#output" ).text(value);
+    });
+
+    $('#plus').click(function(){
+        var value = $(this).val();
+        calculationArr.push(value);
+        $( "#output" ).text(value);
+    });
+
+    $('#minus').click(function(){
+        var value = $(this).val();
+        calculationArr.push(value);
+        $( "#output" ).text(value);
+    });
+
+    $('#divide').click(function(){
+        var value = $(this).val();
+        calculationArr.push(value);
+        $( "#output" ).text(value);
+    });
+
+    $('#times').click(function(){
+        var value = $(this).val();
+        calculationArr.push(value);
+        $( "#output" ).text(value);
     });
 
     $('#is').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        alert(calculationArr);
+
+        var resultS = calculationArr.join('');
+        resultS = eval(resultS);
+        var output = String(resultS);
+        $( "#output" ).text(output);
+
+
+
     });
-
-
 });
 
 
