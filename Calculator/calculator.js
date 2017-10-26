47741673a749565aaa1c80fee25d88820339d354
huijse <michiel.huijse@gmail.com>
2017-10-27 01:29:00 +0200

    Javascript intermediate output completed, plus errors.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,109 +5,118 @@
 $(document).ready(function() {
     // all custom jQuery will go here
     var calculationArr = Array();
+    var numberConcat = Array();
+    var value;
 
     $('#AC').click(function(){
-
         calculationArr = [];
+        numberConcat = [];
         $( "#output" ).text("0");
 
     });
 
     $('#1').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
 
     });
 
     $('#2').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#3').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#4').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#5').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#6').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#7').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#8').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#9').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#0').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        number(value);
     });
 
     $('#plus').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        operator(value);
     });
 
     $('#minus').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        operator(value);
     });
 
     $('#divide').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        operator(value);
     });
 
     $('#times').click(function(){
-        var value = $(this).val();
-        calculationArr.push(value);
-        $( "#output" ).text(value);
+        value = $(this).val();
+        operator(value);
     });
 
     $('#is').click(function(){
-
         var resultS = calculationArr.join('');
         resultS = eval(resultS);
+        resultS = Math.round(resultS * 100) / 100;
         var output = String(resultS);
-        $( "#output" ).text(output);
+        if (output > 999999999) {
+            $( "#output" ).text('Error!');
 
-
-
+        } else {
+            $( "#output" ).text(output);
+        }
     });
+
+    function number(value) {
+        calculationArr.push(value);
+        numberConcat.push(value);
+        if (numberConcat.length > 8) {
+            $( "#output" ).text('Error!');
+        } else {
+            var Result = numberConcat.join('');
+            var output = String(Result);
+            $( "#output" ).text(output);
+        }
+
+    }
+
+    function operator() {
+        calculationArr.push(value);
+        numberConcat = [];
+        $( "#output" ).text(value);
+    }
 });
 
 
